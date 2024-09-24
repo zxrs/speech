c57f203a31bbcceffc1944663c2d554f16f29807
zxrs <tanaka_shin@M1D0004.khi.co.jp>
2024-09-24 13:48:41 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,299 @@
+#![cfg_attr(not(debug_assertions), windows_subsytem = "windows")]
+
+use anyhow::{Context, Result};
+use std::char::{decode_utf16, REPLACEMENT_CHARACTER};
+use std::mem;
+use std::path::PathBuf;
+use std::slice;
+use std::sync::mpsc;
+use std::thread;
+use windows::{
+    core::{w, Interface, HSTRING, PCWSTR, PWSTR},
+    Foundation::TypedEventHandler,
+    Media::{
+        Core::MediaSource,
+        Playback::MediaPlayer,
+        SpeechSynthesis::{SpeechSynthesisStream, SpeechSynthesizer},
+    },
+    Storage::Streams::DataReader,
+    Win32::{
+        Foundation::{HWND, LPARAM, LRESULT, RECT, WPARAM},
+        Graphics::Gdi::{GetSysColorBrush, UpdateWindow, COLOR_MENUBAR},
+        System::{LibraryLoader::GetModuleHandleW, WinRT::IBufferByteAccess},
+        UI::{
+            Controls::Dialogs::{GetSaveFileNameW, OPENFILENAMEW},
+            WindowsAndMessaging::{
+                CreateWindowExW, DefWindowProcW, DispatchMessageW, GetClientRect, GetMessageW,
+                GetWindowTextLengthW, GetWindowTextW, MessageBoxW, PostQuitMessage, RegisterClassW,
+                SendMessageW, ShowWindow, TranslateMessage, BS_PUSHBUTTON, CW_USEDEFAULT,
+                ES_AUTOVSCROLL, ES_MULTILINE, ES_WANTRETURN, HMENU, MB_OK, MSG, SW_SHOW,
+                WINDOW_EX_STYLE, WINDOW_STYLE, WM_COMMAND, WM_CREATE, WM_DESTROY, WM_SETTEXT,
+                WNDCLASSW, WS_BORDER, WS_CAPTION, WS_CHILD, WS_MINIMIZEBOX, WS_OVERLAPPED,
+                WS_SYSMENU, WS_TABSTOP, WS_VISIBLE, WS_VSCROLL,
+            },
+        },
+    },
+};
+
+const CLASS_NAME: PCWSTR = w!("speech_window_cls42");
+const ID_PLAY: u16 = 5890;
+const ID_CLEAR: u16 = 5891;
+const ID_SAVE: u16 = 5892;
+static mut EDIT_HWND: Option<HWND> = None;
+
+fn speech_synthesis_stream(source: &[u16]) -> Result<SpeechSynthesisStream> {
+    let source = HSTRING::from_wide(source)?;
+    let synth = SpeechSynthesizer::new()?;
+    let stream = synth.SynthesizeTextToStreamAsync(&source)?.get()?;
+    Ok(stream)
+}
+
+fn speech() -> Result<()> {
+    let text = get_edit_control_text()?;
+    thread::spawn(move || -> Result<()> {
+        let stream = speech_synthesis_stream(&text)?;
+        let player = MediaPlayer::new()?;
+        let media_source = MediaSource::CreateFromStream(&stream, &stream.ContentType()?)?;
+        player.SetSource(&media_source)?;
+        let (tx, rx) = mpsc::channel();
+        let tx_clone = tx.clone();
+        let token_media_ended = player.MediaEnded(&TypedEventHandler::new(move |_, _| {
+            tx_clone.send(()).ok();
+            Ok(())
+        }))?;
+        let token_media_failed = player.MediaFailed(&TypedEventHandler::new(move |_, _| {
+            tx.send(()).ok();
+            Ok(())
+        }))?;
+        player.Play()?;
+        rx.recv()?;
+        player.RemoveMediaEnded(token_media_ended)?;
+        player.RemoveMediaFailed(token_media_failed)?;
+        Ok(())
+    });
+    Ok(())
+}
+
+fn get_save_file_path(hwnd: HWND) -> Result<PathBuf> {
+    let len = 512;
+    let mut buf = vec![0; len];
+    let mut filename = OPENFILENAMEW {
+        lStructSize: mem::size_of::<OPENFILENAMEW>() as _,
+        hwndOwner: hwnd,
+        lpstrFile: PWSTR::from_raw(buf.as_mut_ptr()),
+        lpstrFilter: w!("Wave File (.wav)\0*.wav\0\0"),
+        lpstrDefExt: w!("wav"),
+        nMaxFile: len as _,
+        ..Default::default()
+    };
+    unsafe { GetSaveFileNameW(&mut filename).ok()? };
+    let path: String = decode_utf16(buf.iter().take_while(|v| *v != &0).copied())
+        .map(|r| r.unwrap_or(REPLACEMENT_CHARACTER))
+        .collect();
+    Ok(path.into())
+}
+
+fn save_to_wav(hwnd: HWND) -> Result<()> {
+    let file_path = get_save_file_path(hwnd)?;
+
+    let text = get_edit_control_text()?;
+    let stream = speech_synthesis_stream(&text)?;
+    let reader = DataReader::CreateDataReader(&stream)?;
+    let size = stream.Size()? as u32;
+    reader.LoadAsync(size)?.get()?;
+    let buffer: IBufferByteAccess = reader.ReadBuffer(size)?.cast()?;
+    let ptr = unsafe { buffer.Buffer()? };
+
+    let slice = unsafe { slice::from_raw_parts(ptr, size as usize) };
+    std::fs::write(&file_path, slice)?;
+
+    let file_name = file_path.file_name().context("no file name.")?;
+    let msg = format!("{} を保存しました。", file_name.to_string_lossy());
+    let msg = msg.encode_utf16().chain(Some(0)).collect::<Vec<_>>();
+    unsafe { MessageBoxW(hwnd, PCWSTR(msg.as_ptr()), None, MB_OK) };
+    Ok(())
+}
+
+unsafe extern "system" fn wnd_proc(
+    hwnd: HWND,
+    msg: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    match msg {
+        WM_CREATE => {
+            create(hwnd).ok();
+        }
+        WM_COMMAND => {
+            command(hwnd, wparam).ok();
+        }
+        WM_DESTROY => PostQuitMessage(0),
+        _ => return DefWindowProcW(hwnd, msg, wparam, lparam),
+    }
+    LRESULT::default()
+}
+
+fn loword(dword: u32) -> u16 {
+    ((dword << 16) >> 16) as _
+}
+
+fn get_edit_control_hwnd() -> Result<HWND> {
+    unsafe { EDIT_HWND.context("no edit control hwnd") }
+}
+
+fn get_edit_control_text() -> Result<Vec<u16>> {
+    let hwnd = get_edit_control_hwnd()?;
+    let len = unsafe { GetWindowTextLengthW(hwnd) };
+    let mut buf = vec![0; len as usize + 1];
+    unsafe { GetWindowTextW(hwnd, &mut buf) };
+    Ok(buf)
+}
+
+fn clear_edit_control_text() -> Result<()> {
+    let hwnd = get_edit_control_hwnd()?;
+    unsafe { SendMessageW(hwnd, WM_SETTEXT, None, None) };
+    Ok(())
+}
+
+fn command(hwnd: HWND, wparam: WPARAM) -> Result<()> {
+    let id = loword(wparam.0 as _);
+
+    if id.eq(&ID_PLAY) {
+        speech()?;
+    } else if id.eq(&ID_CLEAR) {
+        clear_edit_control_text()?;
+    } else if id.eq(&ID_SAVE) {
+        save_to_wav(hwnd)?;
+    }
+
+    Ok(())
+}
+
+fn create_button(
+    hwnd: HWND,
+    label: PCWSTR,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+    id: u16,
+) -> Result<()> {
+    unsafe {
+        CreateWindowExW(
+            WINDOW_EX_STYLE::default(),
+            w!("BUTTON"),
+            label,
+            WS_CHILD | WS_VISIBLE | WINDOW_STYLE(BS_PUSHBUTTON as _),
+            x,
+            y,
+            width,
+            height,
+            hwnd,
+            HMENU(id as _),
+            None,
+            None,
+        )?
+    };
+    Ok(())
+}
+
+fn create_play_button(hwnd: HWND) -> Result<()> {
+    create_button(hwnd, w!("再生"), 10, 10, 100, 30, ID_PLAY)?;
+    Ok(())
+}
+
+fn create_clear_button(hwnd: HWND) -> Result<()> {
+    create_button(hwnd, w!("クリア"), 120, 10, 100, 30, ID_CLEAR)?;
+    Ok(())
+}
+
+fn create_save_button(hwnd: HWND) -> Result<()> {
+    create_button(hwnd, w!("保存"), 230, 10, 100, 30, ID_SAVE)?;
+    Ok(())
+}
+
+fn create_edit(hwnd: HWND) -> Result<()> {
+    let rc = unsafe {
+        let mut rc = RECT::default();
+        GetClientRect(hwnd, &mut rc)?;
+        rc
+    };
+    let hwnd = unsafe {
+        CreateWindowExW(
+            WINDOW_EX_STYLE::default(),
+            w!("EDIT"),
+            None,
+            WINDOW_STYLE((ES_MULTILINE | ES_WANTRETURN | /*ES_AUTOHSCROLL|*/ ES_AUTOVSCROLL) as _)
+                | WS_CHILD
+                | WS_VISIBLE
+                | WS_BORDER
+                | WS_TABSTOP
+                //| WS_HSCROLL,
+            | WS_VSCROLL,
+            0,
+            50,
+            rc.right,
+            rc.bottom - 50,
+            hwnd,
+            None,
+            GetModuleHandleW(None)?,
+            None,
+        )?
+    };
+    unsafe { EDIT_HWND = Some(hwnd) };
+    Ok(())
+}
+
+fn create(hwnd: HWND) -> Result<()> {
+    create_play_button(hwnd)?;
+    create_clear_button(hwnd)?;
+    create_save_button(hwnd)?;
+    create_edit(hwnd)?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let wnd_class = WNDCLASSW {
+        lpfnWndProc: Some(wnd_proc),
+        lpszClassName: CLASS_NAME,
+        hbrBackground: unsafe { GetSysColorBrush(COLOR_MENUBAR) },
+        ..Default::default()
+    };
+
+    unsafe { RegisterClassW(&wnd_class) };
+
+    let hwnd = unsafe {
+        CreateWindowExW(
+            WINDOW_EX_STYLE::default(),
+            CLASS_NAME,
+            w!("speech"),
+            WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_VISIBLE | WS_MINIMIZEBOX,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            600,
+            480,
+            None,
+            None,
+            None,
+            None,
+        )?
+    };
+
+    unsafe { ShowWindow(hwnd, SW_SHOW).ok()? };
+    unsafe { UpdateWindow(hwnd).ok()? };
+
+    let mut msg = MSG::default();
+
+    loop {
+        if !unsafe { GetMessageW(&mut msg, None, 0, 0) }.as_bool() {
+            break;
+        }
+
+        unsafe {
+            let _ = TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+
+    Ok(())
+}
